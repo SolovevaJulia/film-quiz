3529e2c8a2624692f8694bb4d4649653ed76697d
SolovevaJulia <88616487+SolovevaJulia@users.noreply.github.com>
2021-11-10 15:01:17 +0300

    forth commit

--- main.js
+++ main.js
@@ -1,39 +1,55 @@
-// Кнопки
+// Первый вопрос
 
-const answer = document.querySelectorAll('.button-answer');
-      
-answer.forEach(item => {
+const firstAnswer = document.querySelectorAll('.first-button-answer'),
+      firstQuestion = document.querySelector('.first-question'),
+      firstImage = document.querySelector('.first-film-picture');
+
+firstAnswer.forEach(item => {
    item.addEventListener('click', (e) => {
       if (e.target.value == "true") {
          e.target.classList.add('button-green');
+         firstQuestion.style.display = 'none';
+         firstImage.style.display = 'block';
       } else {
          e.target.classList.add('button-red');
+         
       }
    });
 });
 
-// Первый вопрос
-const firstQuestion = document.querySelector('.first-question'),
-      firstImage = document.querySelector('.first-film-picture');
 
-answer.forEach(item => {
+// Второй вопрос
+const secondAnswer = document.querySelectorAll('.second-button-answer'),
+      secondQuestion = document.querySelector('.second-question'),
+      secondImage = document.querySelector('.second-film-picture');
+
+secondAnswer.forEach(item => {
    item.addEventListener('click', (e) => {
       if (e.target.value == "true") {
-         firstQuestion.style.display = 'none';
-         firstImage.style.display = 'block';
-      } 
+         e.target.classList.add('button-green');
+         secondQuestion.style.display = 'none';
+         secondImage.style.display = 'block';
+      } else {
+         e.target.classList.add('button-red');
+         
+      }
    });
 });
 
-// Второй вопрос
-const secondQuestion = document.querySelector('.second-question'),
-      secondImage = document.querySelector('.second-film-picture');
+// Третий вопрос
+const thirdAnswer = document.querySelectorAll('.third-button-answer'),
+      thirdQuestion = document.querySelector('.third-question'),
+      thirdImage = document.querySelector('.third-film-picture');
 
-answer.forEach(item => {
+thirdAnswer.forEach(item => {
    item.addEventListener('click', (e) => {
       if (e.target.value == "true") {
-         secondQuestion.style.display = 'none';
-         secondImage.style.display = 'block';
-      } 
+         e.target.classList.add('button-green');
+         thirdQuestion.style.display = 'none';
+         thirdImage.style.display = 'block';
+      } else {
+         e.target.classList.add('button-red');
+         
+      }
    });
 });
